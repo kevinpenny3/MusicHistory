b0151a2bc37410c94c8c0f7b7fa451258d7ea5d2
unknown <kevin.penny3@yahoo.com>
2020-03-18 16:02:26 -0500

    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿Select * from Genre
+
+Insert Into Artist (ArtistName, YearEstablished) VALUES ('Cohort37', '2010')
+
+--INSERT INTO Song(Title, SongLength, ReleaseDate, GenreId) VALUES ('', '', '', '');
+--INSERT INTO album() VALUES ('', '');
+
+Select * from Artist
